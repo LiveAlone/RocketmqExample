bc3c62bbb070b0992a0ad334c98f4ab129d74759
qijunyao <qijunyao@xiaohongshu.com>
2019-04-18 10:39:15 +0800

    support rocket mq basic content

---
+++ src/main/java/org/yqj/rocketmq/demo/docs/order/OrderedProducer.java
@@ -0,0 +1,47 @@
+package org.yqj.rocketmq.demo.docs.order;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.util.List;
+
+/**
+ * Description:
+ *
+ * @author yaoqijun
+ * @date 2019-04-17
+ * Email: yaoqijunmail@foxmail.com
+ */
+public class OrderedProducer {
+    public static void main(String[] args) throws Exception {
+        //Instantiate with a producer group name.
+        MQProducer producer = new DefaultMQProducer("example_group_name");
+        //Launch the instance.
+        producer.start();
+        String[] tags = new String[] {"TagA", "TagB", "TagC", "TagD", "TagE"};
+        for (int i = 0; i < 100; i++) {
+            int orderId = i % 10;
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("TopicTestjjj", tags[i % tags.length], "KEY" + i,
+                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+            SendResult sendResult = producer.send(msg, new MessageQueueSelector() {
+                @Override
+                public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+                    System.out.println("total queue size is " + mqs.size());
+                    Integer id = (Integer) arg;
+                    int index = id % mqs.size();
+                    return mqs.get(index);
+                }
+            }, orderId);
+
+            System.out.printf("%s%n", sendResult);
+        }
+        //server shutdown
+        producer.shutdown();
+    }
+}
--- src/main/java/org/yqj/rocketmq/demo/docs/quickstart/Consumer.java
+++ src/main/java/org/yqj/rocketmq/demo/docs/quickstart/Consumer.java
@@ -1,34 +1,32 @@
-package org.yqj.rocketmq.demo.docs.simple;
+package org.yqj.rocketmq.demo.docs.quickstart;
 
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.MessageExt;
 
 import java.util.List;
 
 /**
- * Description: 队列内容监听消费
+ * Description:
  *
  * @author yaoqijun
- * @date 2019-04-17
+ * @date 2019-04-18
  * Email: yaoqijunmail@foxmail.com
  */
 public class Consumer {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name");
-
         consumer.setNamesrvAddr("localhost:9876");
-
-        consumer.setVipChannelEnabled(false);
-
         consumer.subscribe("TopicTest", "*");
-
         consumer.registerMessageListener(new MessageListenerConcurrently() {
+
             @Override
-            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                                                            ConsumeConcurrentlyContext context) {
                 System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
                 return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             }
@@ -36,6 +34,7 @@ public class Consumer {
 
         consumer.start();
 
-        System.out.println("Consumer Started.%n");
+        System.out.printf("Consumer Started.%n");
+
     }
 }
